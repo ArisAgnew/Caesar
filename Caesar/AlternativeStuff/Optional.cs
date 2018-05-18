50dd0726775579ec0e72c2653e7541fcf9cceeb8
ArisAgnew <aris.sektor@gmail.com>
2018-05-19 01:10:54 +0300

    [created/released] Released overload extention method called OrElseGetCustomized

--- Caesar/AlternativeStuff/Optional.cs
+++ Caesar/AlternativeStuff/Optional.cs
@@ -34,8 +34,11 @@ namespace Caesar.AlternativeStuff
         public T GetCustomized(Func<T, T> getCustomized) => getCustomized.RequireNonNull()(value);
         public U GetCustomized<U>(Func<T, U> getCustomized) => getCustomized.RequireNonNull()(value);
 
-        public T OrElse(T other) => HasValue ? value : other;
-        public T OrElseGet(Func<T> getOther) => HasValue ? value : getOther();
+        public T OrElse(T other) => HasValue ? value : other.RequireNonNull();
+        public T OrElseGet(Func<T> getOther) => HasValue ? value : getOther.RequireNonNull()();
+        public T OrElseGetCustomized(Func<T, T> elseGetCustomized) => HasValue ? value : elseGetCustomized.RequireNonNull()(value);
+        public dynamic OrElseGetCustomized<U>(Func<T, dynamic> elseGetCustomized) => HasValue ? value : elseGetCustomized.RequireNonNull()(value);
+        
         public T OrElseThrow<E>(Func<E> exceptionSupplier) where E : Exception => HasValue ? value : throw exceptionSupplier();
 
         public static explicit operator T(Optional<T> optional) => optional.RequireNonNull().Get();
