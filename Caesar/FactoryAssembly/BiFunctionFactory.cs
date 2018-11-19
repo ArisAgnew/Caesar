2033e1666862272fbdbc0cb0defcc1a7232ee2cc
Aris <aris.sektor@gmail.com>
2018-11-19 19:22:02 +0300

    [created] BiFunctionFactory class has been created

---
+++ Caesar/FactoryAssembly/BiFunctionFactory.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Caesar.AlternativeStuff;
+
+namespace Caesar.FactoryAssembly
+{
+    public static class BiFunctionFactory
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T1Input"></typeparam>
+        /// <typeparam name="T2Input"></typeparam>
+        /// <typeparam name="TIntermediate"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="after"></param>
+        /// <param name="before"></param>
+        /// <returns></returns>
+        public static Func<T1Input, T2Input, TOutput> BackwardCompose<T1Input, T2Input, TIntermediate, TOutput>
+            (this Func<TIntermediate, TOutput> after, Func<T1Input, T2Input, TIntermediate> before) =>
+            (T1Input t1, T2Input t2) => after.RequireNonNull().Invoke(before.RequireNonNull().Invoke(t1, t2));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T1Input"></typeparam>
+        /// <typeparam name="T2Input"></typeparam>
+        /// <typeparam name="TIntermediate"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="before"></param>
+        /// <param name="after"></param>
+        /// <returns></returns>
+        public static Func<T1Input, T2Input, TOutput> ForwardCompose<T1Input, T2Input, TIntermediate, TOutput>
+            (this Func<T1Input, T2Input, TIntermediate> before, Func<TIntermediate, TOutput> after) =>
+            (T1Input t1, T2Input t2) => after.BackwardCompose(before).Invoke(t1, t2);
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T1Input"></typeparam>
+        /// <typeparam name="T2Input"></typeparam>
+        /// <typeparam name="TIntermediate"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="after"></param>
+        /// <param name="before"></param>
+        /// <returns></returns>
+        public static Func<T1Input, Func<T2Input, TOutput>> BackwardComposeCurried<T1Input, T2Input, TIntermediate, TOutput>
+            (this Func<TIntermediate, TOutput> after, Func<T1Input, T2Input, TIntermediate> before) =>
+            (T1Input t1) => (T2Input t2) => after.Invoke(before.Invoke(t1, t2));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T1Input"></typeparam>
+        /// <typeparam name="T2Input"></typeparam>
+        /// <typeparam name="TIntermediate"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="before"></param>
+        /// <param name="after"></param>
+        /// <returns></returns>
+        public static Func<T1Input, Func<T2Input, TOutput>> ForwardComposeCurried<T1Input, T2Input, TIntermediate, TOutput>
+            (this Func<T1Input, T2Input, TIntermediate> before, Func<TIntermediate, TOutput> after) =>
+            (T1Input t1) => (T2Input t2) => after.BackwardComposeCurried(before).Invoke(t1).Invoke(t2);
+    }
+}
--- Caesar/FactoryAssembly/FunctionFactory.cs
+++ Caesar/FactoryAssembly/FunctionFactory.cs
@@ -33,35 +33,7 @@ namespace Caesar.FactoryAssembly
         public static Func<TInput, TOutput> ForwardCompose<TInput, TIntermediate, TOutput>
             (this Func<TInput, TIntermediate> before, Func<TIntermediate, TOutput> after) => 
             (TInput input) => after.BackwardCompose(before).Invoke(input);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T1Input"></typeparam>
-        /// <typeparam name="T2Input"></typeparam>
-        /// <typeparam name="TIntermediate"></typeparam>
-        /// <typeparam name="TOutput"></typeparam>
-        /// <param name="after"></param>
-        /// <param name="before"></param>
-        /// <returns></returns>
-        public static Func<T1Input, T2Input, TOutput> BackwardCompose<T1Input, T2Input, TIntermediate, TOutput>
-            (this Func<TIntermediate, TOutput> after, Func<T1Input, T2Input, TIntermediate> before) =>
-            (T1Input t1, T2Input t2) => after.RequireNonNull().Invoke(before.RequireNonNull().Invoke(t1, t2));
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T1Input"></typeparam>
-        /// <typeparam name="T2Input"></typeparam>
-        /// <typeparam name="TIntermediate"></typeparam>
-        /// <typeparam name="TOutput"></typeparam>
-        /// <param name="before"></param>
-        /// <param name="after"></param>
-        /// <returns></returns>
-        public static Func<T1Input, T2Input, TOutput> ForwardCompose<T1Input, T2Input, TIntermediate, TOutput>
-            (this Func<T1Input, T2Input, TIntermediate> before, Func<TIntermediate, TOutput> after) =>
-            (T1Input t1, T2Input t2) => after.BackwardCompose(before).Invoke(t1, t2);
-
+        
         /// <summary>
         /// 
         /// </summary>
