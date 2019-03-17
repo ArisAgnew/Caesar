412e8433cd1757264fda62310e2e26cdac63ad41
ArisAgnew <aris.sektor@gmail.com>
2019-03-17 18:00:48 +0300

    [created] Selenium Core

---
+++ SeleniumCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace SeleniumCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
